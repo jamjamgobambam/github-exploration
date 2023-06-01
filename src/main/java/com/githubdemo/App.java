5a26561b0e00f0f4975823f007f14c0d867dc9d9
jamjamgobambam <jamila.khawaja85@gmail.com>
2023-06-01 16:59:13 -0400

    add new line to output

--- src/main/java/com/githubdemo/App.java
+++ src/main/java/com/githubdemo/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         
         System.out.println("Welcome to GitHub!");
-        
+        System.out.println("Another line added to the code");
+
     }
 }
